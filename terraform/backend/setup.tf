00397fde4f76fa1719a8f57a43b7ea687208eeef
Berko10 <barberko1@gmail.com>
2025-04-16 13:38:06 +0300

    .

--- terraform/backend/setup.tf
+++ terraform/backend/setup.tf
@@ -2,22 +2,34 @@ provider "aws" {
   region = var.aws_region
 }
 
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
 resource "aws_s3_bucket" "tf_state" {
-  bucket = var.tf_bucket_name
+  bucket = "my-devops-tf-state-bucket-${random_id.bucket_suffix.hex}"
   force_destroy = true
-  versioning {
-    enabled = true
-  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 
   tags = {
     Name = "Terraform State Bucket"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "tf_state_versioning" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state_sse" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
 }
 
--- terraform/backend/variables.tf
+++ terraform/backend/variables.tf
@@ -3,11 +3,6 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "tf_bucket_name" {
-  description = "S3 bucket for Terraform state"
-  default = "my-devops-tf-state-bucket"
-}
-
 variable "dynamo_table_name" {
   description = "DynamoDB table for Terraform state locking"
   default = "terraform-locks"
