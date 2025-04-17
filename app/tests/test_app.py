bb5a7b08845a37e5d88fc5c673d8b22aad951e95
Berko10 <97841121+Berko10@users.noreply.github.com>
2025-04-17 17:44:11 +0300

    Update test_app.py

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -1,5 +1,7 @@
-import pytest
-from app import app  # כאן נניח ש-Flask הוגדר בקובץ app.py שלך
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from app import app as flask_app
 
 @pytest.fixture
 def client():
