ccd45b6e31466d71fd722adcd8ea1461b9d14002
Berko10 <barberko1@gmail.com>
2025-04-17 14:22:07 +0300

    alb

--- terraform/main/outputs.tf
+++ terraform/main/outputs.tf
@@ -1,7 +1,7 @@
 output "alb_dns" {
-  description = "Load balancer DNS"
-  value       = module.alb.dns_name
+  value = aws_lb.devops_alb.dns_name
 }
+
 output "ecr_repo_name" {
   value = aws_ecr_repository.app_repo.name
 }
