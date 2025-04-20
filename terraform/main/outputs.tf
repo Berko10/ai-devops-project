80b079840e88689ddaddfaec757ecf8c818d2b73
Berko10 <97841121+Berko10@users.noreply.github.com>
2025-04-20 12:18:01 +0300

    Update outputs.tf

--- terraform/main/outputs.tf
+++ terraform/main/outputs.tf
@@ -1,7 +1,22 @@
-output "alb_dns" {
-  value = aws_lb.devops_alb.dns_name
+# =========================
+# OUTPUTS
+# =========================
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
 
-output "ecr_repo_name" {
-  value = aws_ecr_repository.app_repo.name
+output "public_subnet_ids" {
+  value = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+}
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.app_repo.repository_url
+}
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.devops_cluster.name
+}
+
+output "alb_dns_name" {
+  value = aws_lb.devops_alb.dns_name
 }
