e4d62604a4c979f30307aa78c9c0da812746cd96
Berko10 <97841121+Berko10@users.noreply.github.com>
2025-04-20 14:33:15 +0300

    Update variables.tf

--- terraform/backend/variables.tf
+++ terraform/backend/variables.tf
@@ -7,3 +7,8 @@ variable "dynamo_table_name" {
   description = "DynamoDB table for Terraform state locking"
   default = "terraform-locks"
 }
+
+variable "s3_bucket_name" {
+  description = "S3 bucket for Terraform state file"
+  default = "ai-devops-project-tf-state"
+}
