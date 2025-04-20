643af8d28a5bb9d7b16a27d5b6ac0516faf893a9
Berko10 <97841121+Berko10@users.noreply.github.com>
2025-04-20 12:36:49 +0300

    Update outputs.tf

--- terraform/main/outputs.tf
+++ terraform/main/outputs.tf
@@ -13,6 +13,10 @@ output "ecr_repository_url" {
   value = aws_ecr_repository.app_repo.repository_url
 }
 
+output "ecr_repo_name" {
+  value = aws_ecr_repository.app_repo.repository_url
+}
+
 output "ecs_cluster_name" {
   value = aws_ecs_cluster.devops_cluster.name
 }
