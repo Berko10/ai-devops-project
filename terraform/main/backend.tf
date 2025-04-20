79aadbce30d4069a99f0cafe6bf3d9ddb04b296f
Berko10 <97841121+Berko10@users.noreply.github.com>
2025-04-20 12:19:54 +0300

    Delete terraform/main/backend.tf

--- terraform/main/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "ai-devops-project-tf-state"
-    key            = "main/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "ai-devops-project-locks"
-  }
-}
